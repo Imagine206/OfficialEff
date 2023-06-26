f8a1cce69b3a385c6b79b36c8337700d7c775eb3
Abdisalam <abdisalamfresh@gmail.com>
2023-06-25 22:26:09 -0700

    Added images and color theme to the page and some js for the img will need to work on the about section

--- src/script.js
+++ src/script.js
@@ -11,3 +11,28 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+const homeImg = document.getElementById('homeImg');
+const images = [
+        "/src/images/79d69d_5653c78281ad49ed93fd4dbfa07d8d2c~mv2.webp",  
+        "/src/images/effImgs.webp",
+        "/src/images/fahmi1eff.webp",
+        "/src/images/fahmi2eff.webp",
+        "/src/images/ahmedeff.webp",
+        "/src/images/Screenshot 2023-06-25 221850.png",
+        "/src/images/Screenshot 2023-06-25 221952.png"
+    ];
+let currentIndex = 0;
+const switchImage = () => {
+    homeImg.classList.add('fade-out');
+
+    setTimeout(() => {
+        homeImg.src = images[currentIndex];
+        currentIndex = (currentIndex + 1) % images.length;
+        homeImg.classList.remove('fade-out');
+        homeImg.classList.add('fade-in');
+    }, 500);
+
+}
+
+setInterval(switchImage, 3000);
